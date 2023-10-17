a3623ab1c8594b58077695dfe173b6a94023b9b5
Lin Hui <Lin@HaAir.lan>
2023-10-17 17:01:11 -0400

    lint

--- main.py
+++ main.py
@@ -9,10 +9,13 @@ def fetch_data_for_year_2015(cursor):
             ped.State,
             SUM(ped.ObservedDeaths) AS Total_ObservedDeaths,
             SUM(ped.PotentiallyExcessDeaths) AS Total_PotentiallyExcessDeaths,
-            AVG(ped.PercentPotentiallyExcessDeaths) AS Avg_PercentPotentiallyExcessDeaths,
+            AVG(ped.PercentPotentiallyExcessDeaths) AS
+                    Avg_PercentPotentiallyExcessDeaths,
             hic."UninsuredRate(2015)" AS UninsuredRate_2015,
-            hic."HealthInsuranceCoverageChange(2010-2015)" AS HealthInsuranceCoverageChange_2010_2015,
-            hic."EmployerHealthInsuranceCoverage(2015)" AS EmployerCoverage_2015
+            hic."HealthInsuranceCoverageChange(2010-2015)" AS
+                    HealthInsuranceCoverageChange_2010_2015,
+            hic."EmployerHealthInsuranceCoverage(2015)" AS
+                    EmployerCoverage_2015
         FROM
             Potentially_excess_deaths ped
         JOIN
